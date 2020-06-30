55103e438f89106c481ef22883e5f7915f0822fa
unknown <mmffrr2501@gmail.com>
2020-06-30 19:31:36 +0200

    Stairs Updated file

--- Stairs/src/Stairs.java
+++ Stairs/src/Stairs.java
@@ -1,13 +1,22 @@
+/*
+ * <h1>Stairs<h1>
+ * this code is to draw Stairs using one integer number (n)
+ * the base and height are both equal to n, and the image is drawn using # symbols and spaces.
+The last line is not preceded by any spaces.
+ * */
 import java.util.Scanner;
 
 public class Stairs {
 
 	public static void main(String[] args) {
 		
+		//This scanner (row) to to get an input from the user to control row's number:
 		Scanner scan = new Scanner(System.in);
 		int row = scan.nextInt();
 		
+		//"k" is to use in the second for loop to control how many "#"s we want to print out:
 		int  k = 0;
+		
 		//This for loop to control rows' number
         for(int i = 1; i <= row; ++i, k = 0)
         {
@@ -19,7 +28,8 @@ public class Stairs {
           //This for loop to control "#"s number
             while(k !=  i )
             {
-                System.out.print(" #");
+            	System.out.print(" #");
+            	
                 ++k;
            
             }
