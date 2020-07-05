53b1119032b787dc9355d0ca70108592525870ca
unknown <mmffrr2501@gmail.com>
2020-07-05 02:34:59 +0200

    OK

--- Sort_Numbers/src/Sort.java
+++ Sort_Numbers/src/Sort.java
@@ -1,6 +1,17 @@
+/*
+ * <h1>Sort Numbers (Descending or Ascending) <h1>
+ * this simply asks the user to input how many numbers to be sorted
+ * then ask the user the numbers
+ * after that ask the user to chose the type of sorting:
+ * 1-Descending
+ * 2-Ascending 
+ * then print out the sorted numbers.
+ * */
+
 import java.util.Scanner;
 
 public class Sort {
+	//Descending method:
 	public static void descending  (int numbers1[],int length)
 	{
 		int temp=0;
@@ -18,6 +29,7 @@ public class Sort {
 		}
 		
 	}
+	//Ascending method:
 	public static void ascending  (int numbers1[],int length)
 	{
 		int temp=0;
@@ -37,17 +49,22 @@ public class Sort {
 	}
 
 	public static void main(String[] args) {
-		int temp=0;
+	
 		Scanner scan=new Scanner(System.in);
 		System.out.println("How many numbers do you want to sort?:");
 		int length=scan.nextInt();
+		
 		Scanner scan1=new Scanner(System.in);
 		int [] numbers= new int [length];
+		
 		System.out.println("Enter "+length+" numbers :");
+		//this for loop is to take the numbers:
 		for (int i=0;i<length;i++)
 		{
 			numbers[i]=scan1.nextInt();
 		}
+		
+		//chose sorting type:
 		Scanner scan2= new Scanner(System.in);
 		System.out.println("1/ For descending sorting.");
 		System.out.println("2/ For ascending  sorting.");
@@ -56,7 +73,9 @@ public class Sort {
 			descending(numbers,length);
 		else if(type==2)
 			ascending(numbers,length);
-		
+		scan.close();
+		scan1.close();
+		scan2.close();
 	}
 
 }
