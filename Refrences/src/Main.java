16dc2f843e6dfcbd40b0df3644164e6f39c28e3c
unknown <mmffrr2501@gmail.com>
2020-07-11 01:01:53 +0200

    test

--- Refrences/src/Main.java
+++ Refrences/src/Main.java
@@ -13,7 +13,8 @@ public class Main {
 		//test the second reference "anotherHouse":
 		System.out.println(anotherHouse.getColor());//Blue
 		
-		//Change the second reference "anotherHouse" String value:
+		//Change the second reference "anotherHouse" String value which will change
+		//blueHouse value too:
 		anotherHouse.setColor("Black");
 		//test both references:
 		System.out.println(blueHouse.getColor());//Black , it changed.
@@ -23,10 +24,15 @@ public class Main {
 		House greenHouse=new House("Green");
 		//test for greenHouse:
 		System.out.println(greenHouse.getColor());//green
-		//change anotherHouse color using green refernce:
+		//change anotherHouse color using green reference:
 		anotherHouse=greenHouse;
 		//now let's test anotherHouse ,it is supposed to get Green :
 		System.out.println(anotherHouse.getColor());//Green, it worked.
+		
+		//testing them all:
+		System.out.println(blueHouse.getColor());//Black .
+		System.out.println(anotherHouse.getColor());//Green since i assigned it in line 28
+		System.out.println(greenHouse.getColor());//Green
 	}
 
 }
